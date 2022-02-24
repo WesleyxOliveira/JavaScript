68da809815bf237c26d894c44e06cff4a879d8ae
WesleyxOliveira <92177435+WesleyxOliveira@users.noreply.github.com>
2022-02-24 20:06:06 -0300

    Initial commit

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu veiculo é de ${vel} km/h`)
+if (vel > 60) { //Condição Simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre utilizando o sinto de segurança!')
---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,29 @@
+function calcular() {
+    var n1 = document.getElementById('inicio')
+    var n2 = document.getElementById('fim')
+    var n3 = document.getElementById('passo')
+    var res = document.getElementById('res')
+    var inicio = Number(n1.value)
+    var fim = Number(n2.value)
+    var passo = Number(n3.value)
+    if (inicio == 0 || fim == 0) {        
+        window.alert('Impossível contar')
+    }else if (passo == 0) {
+        window.alert('Passo inválido! Considerando PASSO 1.')
+        passo = passo +=1
+        res.innerHTML += ("<p>Contando: </p>")
+        while (inicio <= fim) {
+            res.innerHTML += (`${inicio} &#x1F449`)
+            inicio = inicio += passo
+        }
+        res.innerHTML += ('&#x1F3C1')
+    }else {
+        res.innerHTML += ("<p>Contando: </p>")
+        while (inicio <= fim) {
+            res.innerHTML += (`${inicio} &#x1F449`)
+            inicio = inicio += passo
+        }
+        res.innerHTML += ('&#x1F3C1')
+    }
+}
+
