c1d3cdfbb81b9d8237d9e996443ff00ad0430d50
WesleyxOliveira <92177435+WesleyxOliveira@users.noreply.github.com>
2022-02-24 21:38:47 -0300

    criando ex17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,8 @@
-window.alert('Olá')
+function gerar() {
+    var n = document.getElementById('entrada')
+    var tab = document.getElementById('tabuada')
+    var um = document.getElementById('um')
+    var numero = Number(n.value)
+    um.innerHTML = (`${numero} x 1 = ${numero*1}`)
+    dois.innerHTML = (`${numero} x 1 = ${numero*2}`)
+}
