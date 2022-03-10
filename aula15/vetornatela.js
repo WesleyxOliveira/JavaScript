4297ebb13c33efbec4ed5ea8696aca7614302085
WesleyxOliveira <92177435+WesleyxOliveira@users.noreply.github.com>
2022-03-10 19:31:06 -0300

    aula15

---
+++ aula15/vetornatela.js
@@ -0,0 +1,41 @@
+let valores = [8, 1, 7, 4, 2, 9]
+//console.log(valores)
+/*
+console.log(valores[0])
+console.log(valores[1])
+console.log(valores[2])
+console.log(valores[3])
+console.log(valores[4])
+*/
+/*
+var pos = 0
+do {
+    console.log(valores[pos])
+    pos++
+}while (pos <= valores.length)
+*/
+
+/*
+for (let pos = 0; pos <= valores.length; pos++) {
+    console.log(valores[pos])
+}
+*/
+
+/*
+var pos = 0
+console.log(valores)
+while (pos <= valores.length) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+    pos++ 
+}
+*/
+
+/*
+valores.sort()
+console.log(valores)
+for(let pos in valores) {
+    console.log(`Na posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+console.log(valores.indexOf(3))
+valores.indexOf(1)
